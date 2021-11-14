242be09ee05314525ae729e4f05610c87e49f0a8
Dave O'Connor <git@mail.andvari.net>
2021-11-14 19:36:25 +0000

    Well, it published to MQTT, but it's not pretty.

--- mqttbow.py
+++ mqttbow.py
@@ -1,21 +1,41 @@
+import time
 import asyncio
 import logging
+import argparse
 import asynckeybow
+from hbmqtt.client import MQTTClient, ConnectException
 
 
-async def process_keystrokes(ki, key_q):
+async def process_keystrokes(ki, mqtt, base_topic, key_q):
     while True:
         keypress = await key_q.get()
         if keypress:
-            print('Key Press: %s' % (keypress, ))
+            topic = base_topic + str(keypress[0]) + '/%s' % (keypress[1].name)
+            payload = str(time.time())
+            logging.debug('MQTT Publish: [%s]: %s' % (topic, payload))
+            await mqtt.publish(topic, payload.encode('ascii'))
+
 
 
 async def main():
     logging.basicConfig(level=logging.DEBUG)
-    key_q = asyncio.Queue()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--implementation', default='keybow', help='Keyboard implementation. [keybow|simulated]')
+    parser.add_argument('--mqtt_broker', default='mqtt:1883', help='MQTT broker host:port')
+    parser.add_argument('--mqtt_topic', default='keybow/press/', help='MQTT base topic')
+    parser.add_argument('--mqtt_user', default=None, help='MQTT user')
+    parser.add_argument('--mqtt_pass', default=None, help='MQTT password')
+    args = parser.parse_args()
 
-    # ki = asynckeybow.KeyInterface(asynckeybow.Implementation.KEYBOW)
-    ki = asynckeybow.KeyInterface(asynckeybow.Implementation.SIMULATED)
+
+    # Setup the keybow interface (or simulation).
+    if args.implementation == 'keybow':
+        ki = asynckeybow.KeyInterface(asynckeybow.Implementation.KEYBOW)
+    elif args.implementation == 'simulated':
+        ki = asynckeybow.KeyInterface(asynckeybow.Implementation.SIMULATED)
+    else:
+        logging.error('--implementation must be [keybow|simulated]')
+        return
 
     script = [
         'down 1',
@@ -30,7 +50,26 @@ async def main():
 
     seq_l = asynckeybow.KeySequenceListener(ki, listen_for=[asynckeybow.KeySequence.SINGLE, asynckeybow.KeySequence.HOLD])
 
-    await asyncio.gather(seq_l.produce(key_q), process_keystrokes(ki, key_q))
+
+    # Set up the MQTT Publisher
+    mqtt = MQTTClient()
+
+    if args.mqtt_user:
+        mqtt_url = 'mqtt://%s:%s@%s' % (args.mqtt_user, args.mqtt_pass, args.mqtt_broker)
+        logging.debug('Connecting to MQTT broker at %s with username %s' % (args.mqtt_broker, args.mqtt_user))
+    else:
+        mqtt_url = 'mqtt://%s' % (args.mqtt_broker)
+        logging.debug('Connecting Anonymously to MQTT broker at %s' % (args.mqtt_broker))
+
+    try:
+        await mqtt.connect(mqtt_url)
+    except ConnectException as e:
+        logging.error('Error connecting to MQTT broker: %s' % str(e))
+        return
+
+    key_q = asyncio.Queue()
+
+    await asyncio.gather(seq_l.produce(key_q), process_keystrokes(ki, mqtt, args.mqtt_topic, key_q))
 
 
 if __name__ == '__main__':
